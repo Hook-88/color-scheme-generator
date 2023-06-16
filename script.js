05decd76e1ec8afae2c39bd3d5e9bcb53902b5b1
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-06-16 15:29:30 -0500

    Add logic to display scheme

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const form = document.querySelector('form')
 const colorPickerEl = document.getElementById("color-picker")
 const colorModeEl = document.getElementById("color-mode")
-
-
-// fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=monochrome&count=5")
-//   .then(res => res.json())
-//   .then(data => console.log(data))
-
+const colorBarsConatiner = document.querySelector("main")
+const hexcodeContainer =  document.querySelector("footer ul")
+let colorsArray = []
 
 form.addEventListener("submit", event => {
   event.preventDefault()
+  colorsArray = []
+  getColors() 
+})
+
+function getColors() {
   fetch(`https://www.thecolorapi.com/scheme?hex=${colorPickerEl.value.slice(1)}&mode=${colorModeEl.value}&count=5`)
     .then(res => res.json())
-    .then(data => {
+    .then(data => {      
       data.colors.forEach(color => {
-        console.log(color.hex.value)
-      });
+        colorsArray.push(color.hex.value)
+      })
+      render()
     })
-  
-})
+}
+
+function render() {
+  let htmlMain = ""
+  let htmlFooter = ""
+  colorsArray.forEach(color => {
+    htmlMain += `
+      <div style="background: ${color}"></div>
+    `
+    htmlFooter += `
+      <li>${color}</li>
+    `
+  })
+  colorBarsConatiner.innerHTML = htmlMain
+  hexcodeContainer.innerHTML = htmlFooter
+}
+
+getColors()
